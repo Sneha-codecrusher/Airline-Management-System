ab4ae731f560b8f29f2f5af04e76d30ff9f2eb5b
Sneha-codecrusher <isnehaag2812@gmail.com>
2024-07-29 19:32:20 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,20 @@
+
+package airlinemanagementsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn(){
+        try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+           c = DriverManager.getConnection("jdbc:mysql://localhost:3306/airlinemanagementsystem", "root", "Sneha@281220");
+           s = c.createStatement();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
